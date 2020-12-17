729bb518009a4cf0cf633693048e294d40fb4d78
osalam <tangbinhlam@gmail.com>
2020-12-17 11:35:08 -0800

    Create and using autoBind Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const ajustDes: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return ajustDes;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -33,13 +45,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @autoBind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
